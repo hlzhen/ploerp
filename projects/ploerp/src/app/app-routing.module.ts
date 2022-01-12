e5459c2c150ca995ddfe08a11571f1245f61d13f
Ale <ploerp@163.com>
2022-01-12 17:26:51 +0800

    优化路由

--- projects/ploerp/src/app/app-routing.module.ts
+++ projects/ploerp/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PloPageNotfoundComponent } from './sys-admin/plo-component/plo-page-notfound/plo-page-notfound.component';
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./sys-admin/sys-admin.module').then(m => m.SysAdminModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./sys-login/sys-login.module').then(m => m.SysLoginModule)
+  },
+  {
+    path: '**',
+    component: PloPageNotfoundComponent
   }
 ];
 
--- projects/ploerp/src/app/sys-admin/sys-admin-routing.module.ts
+++ projects/ploerp/src/app/sys-admin/sys-admin-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { PloPageNotfoundComponent } from './plo-component/plo-page-notfound/plo-page-notfound.component';
 import { routerGuard } from './plo-guard/router-guard';
 import { PloLayoutComponent } from './plo-layout/plo-layout.component';
 import { DashboardComponent } from './plo-pages/dashboard/dashboard.component';
@@ -17,21 +16,13 @@ const routes: Routes = [
       },
       {
         path: 'dashboard',
-        component: DashboardComponent,
+        component: DashboardComponent
       },
       {
         path: 'sys',
         loadChildren: () => import('./plo-pages/sys-setting/sys-setting.module').then(m => m.SysSettingModule)
-      },
+      }
     ]
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./plo-login/plo-login.module').then(m => m.PloLoginModule)
-  },
-  {
-    path: '**',
-    component: PloPageNotfoundComponent
   }
 ]
 
